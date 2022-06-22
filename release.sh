3fbbb8b4949ba00cf32bd36da6dc3ed266d9896f
Zik <vladziku@gmail.com>
2022-06-22 18:42:02 +0300

    Fix

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-yarn run tsup src/index.tsx --format esm,cjs,iife --minify --dts --target es5
+yarn run tsup src/index.tsx --format esm,cjs,iife --minify --dts --target es5 --legacy-output --inject ./react-import.js
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { WidgetComponent } from './components'
 import { render } from 'react-dom'
 
@@ -20,4 +21,3 @@ export const renderWidget = (
 }
 
 export { WidgetComponent as Widget } from './components'
-
